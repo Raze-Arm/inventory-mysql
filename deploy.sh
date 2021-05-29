2fbfa96603e4e9d14431cd3ca365e19bad5a9137
razar <razear007@gmail.com>
2021-05-29 09:21:00 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 
 #fandogh volume add --name vol1 -c 10
 
-#fandogh service apply -f fandogh-mysql-deployment.yml -p DB_PASSWORD=the-secret-way-to-doing-stuff
+fandogh service apply -f fandogh-mysql-deployment.yml -p DB_PASSWORD=the-secret-way-to-doing-stuff
 
 
-fandogh managed-service deploy mysql 5.7 \
-     -c service_name=mega-electric-mysql \
-     -c mysql_root_password=the-secret-way-to-doing-stuff\
-     -c phpmyadmin_enabled=false \
-     -m 500Mi
+#fandogh managed-service deploy mysql 5.7 \
+#     -c service_name=mega-electric-mysql \
+#     -c mysql_root_password=the-secret-way-to-doing-stuff\
+#     -c phpmyadmin_enabled=false \
+#     -m 500Mi
