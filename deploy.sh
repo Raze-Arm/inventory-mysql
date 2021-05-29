198645c46a52be936982cc4aa605241ea108ac77
razar <razear007@gmail.com>
2021-05-29 12:10:46 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 
 #fandogh volume add --name vol1 -c 10
 docker build  -t razear/mega-electric-mysql:latest -t razear/mega-electric-mysql:$SHA -f Dockerfile .
-
+docker push razear/mega-electric-web-app:latest
+docker push razear/mega-electric-web-app:$SHA
 #fandogh service apply -f mysql-deployment.yml -p DB_PASSWORD=the-secret-way-to-doing-stuff
 
 fandogh service deploy  --image razear/mega-electric-mysql --version $SHA --name mega-electric-mysql --internal  -p 3306 -d
