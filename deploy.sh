4e1936e1ab26cb7fc3529d59bb302b777f9cf1c6
razar <razear007@gmail.com>
2021-05-29 09:42:29 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 
 #fandogh volume add --name vol1 -c 10
 
-fandogh service apply -f fandogh-mysql-deployment.yml -p DB_PASSWORD=the-secret-way-to-doing-stuff
+#fandogh service apply -f fandogh-mysql-deployment.yml -p DB_PASSWORD=the-secret-way-to-doing-stuff
+fandogh service apply -f fandogh-mysql-deployment.yml
