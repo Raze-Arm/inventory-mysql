f8cbcdf1f2735f00ff9d16d8cd56df517067ebd0
razar <razear007@gmail.com>
2021-06-06 01:57:52 -0400

    configuring travis yml file to use fandogh yml file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM mysql:8.0
-ENV MYSQL_ROOT_PASSWORD the-secret-way-to-doing-stuff
+ARG MYSQL_ROOT_PASSWORD
+ENV MYSQL_ROOT_PASSWORD ${MYSQL_ROOT_PASSWORD}
 ENV MYSQL_DATABASE inventory
 ADD schema.sql /docker-entrypoint-initdb.d
 EXPOSE 3306
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 
 #fandogh volume add --name vol1 -c 10
-docker build  -t razear/mega-electric-mysql:latest -t razear/mega-electric-mysql:$SHA -f Dockerfile .
+docker build  --build-arg MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -t razear/mega-electric-mysql:latest -t razear/mega-electric-mysql:$SHA -f Dockerfile .
 docker push razear/mega-electric-mysql:latest
 docker push razear/mega-electric-mysql:$SHA
-#fandogh service apply -f mysql-deployment.yml -p DB_PASSWORD=the-secret-way-to-doing-stuff
+fandogh service apply -f mysql-deployment.yml -p SHA=$SHA
 
-fandogh service deploy  --image razear/mega-electric-mysql --version $SHA --name mega-electric-mysql --internal  -p 3306  -d
+#fandogh service deploy  --image razear/mega-electric-mysql --version $SHA --name mega-electric-mysql --internal  -p 3306  -d
