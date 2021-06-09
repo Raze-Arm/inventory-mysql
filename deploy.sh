f14fb902f48a5a3c8206feb6c6e3e1fdcc3aa4ce
razar <razear007@gmail.com>
2021-06-09 08:28:03 -0400

    fix travis build

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,9 @@ docker build  --build-arg -t razear/mega-electric-mysql:latest -t razear/mega-el
 docker push razear/mega-electric-mysql:latest
 docker push razear/mega-electric-mysql:$SHA
 
-fandogh  secret create  \
-          --name mysql-root-password \
-          -t environment-secret \
-          -f SECRET_KEY=$MYSQL_ROOT_PASSWORD \
+ \
 
 
-fandogh service apply -f mysql-deployment.yml -p SHA=$SHA
+fandogh service apply -f mysql-deployment.yml -p SHA=$SHA -p MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
 
 #fandogh service deploy  --image razear/mega-electric-mysql --version $SHA --name mega-electric-mysql --internal  -p 3306  -d
